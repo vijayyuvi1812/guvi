712afe9669922328ffb0aacd93a1cdfefde6d877
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-07-27 11:55:53 +0530

    Create printntime.java

---
+++ beginer/printntime.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter how many times u want to print HELLO");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++){
+			System.out.println("HELLO");
+		}
+	}
+}
