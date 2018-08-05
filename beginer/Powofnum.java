fec73c2eda0a76872fa372b6ef81b3a20276641d
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-08-05 11:27:23 +0530

    Create Powofnum.java

---
+++ beginer/Powofnum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+
+class Rextester
+{  
+    public static void main(String args[])
+    {
+	
+       long n,p,r=1;
+	
+       Scanner sc=new Scanner(System.in);
+
+	     n=sc.nextLong();
+             p=sc.nextLong();
+ 
+       if(n>=0&&p==0)
+       {   
+          r =1;
+       }
+       else if(n==0&&p>=1)
+	{ 
+            r=0;
+	} 
+       else
+	{ 
+              for(int i=1;i<=p;i++)
+	       { 
+                   r=r *n;
+ 	 	}    
+  	 }    
+ 
+    	  System.out.println(r);
+
+    }
+}
