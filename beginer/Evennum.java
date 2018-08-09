fea3e630f4b894c90d0d75e86670f7b81f1ac33a
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-08-09 11:38:20 +0530

    Create Evennum.java

---
+++ beginer/Evennum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class OddNumber {
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+int y=sc.nextInt();
+		for (int i = x; i <= y; i++) {
+			if (i % 2 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
