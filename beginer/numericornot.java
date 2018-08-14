bdd682d0066dfe3342096b068b01163a5ca42407
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-08-14 01:32:11 -0400

    Create numericornot.java

---
+++ beginer/numericornot.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc=new Scanner(System.in);
+		 String string=sc.nextLine();
+        boolean numeric = true;
+
+        try {
+            Double num = Double.parseDouble(string);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println("yes");
+        else
+	System.out.println("No"); 
+        
+	}
+}
