ed42fe7ea8349b410444df603b53c8ac7331947a
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-08-16 10:12:38 +0530

    Create lenstrwithoutspace.java

---
+++ beginer/lenstrwithoutspace.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int sum=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)==' ')
+			{
+				continue;
+			}
+			sum++;
+		}
+		System.out.println(sum);
+	}
+}
