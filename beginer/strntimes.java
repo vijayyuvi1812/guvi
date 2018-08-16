f856d1b16f7e31e80f57de140628e5d3ae725103
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-08-16 12:33:32 +0530

    Create strntimes.java

---
+++ beginer/strntimes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String N=sc.nextLine();
+		int K=sc.nextInt();
+		for(int i=0;i<K;i++){
+			System.out.println(N);
+		}
+	}
+}
