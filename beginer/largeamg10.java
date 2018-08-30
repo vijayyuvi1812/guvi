e3d56496c750c2675bc26350cdf5148eb9f81144
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-08-30 09:58:52 +0530

    Create largeamg10.java

---
+++ beginer/largeamg10.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int a[]=new int[10];
+		for(int i=0;i<10;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int max=a[0];
+		for(int i=0;i<10;i++)
+		{
+			if(max<a[i])
+				max=a[i];
+		}
+		System.out.println(max);
+	}
+}
