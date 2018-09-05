6b3eb97956ad129ec7fc25ed9c292d83dc2f0005
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-09-05 21:19:55 +0530

    Create avgofarray.java

---
+++ beginer/avgofarray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int total=0;
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			total=total+a[i];
+		}
+		int avg=(int) total/n;
+		System.out.println(avg);
+		
+	}
+}
