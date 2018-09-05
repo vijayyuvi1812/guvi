c2b64fc0a319fee4daaa418be4776ae8587cf85f
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-09-05 20:51:31 +0530

    Create noofdigits.java

---
+++ beginer/noofdigits.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int count=0;
+		int num=sc.nextInt();
+		 while(num != 0)
+        {
+            
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println(count);
+	}
+}
