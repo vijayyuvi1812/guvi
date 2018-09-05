999f717013c9a480055db253a27600e22c10936c
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-09-05 15:38:03 +0530

    Create stringconcat.java

---
+++ beginer/stringconcat.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+			String s2=sc.nextLine();
+		System.out.println(s1+s2);
+		
+	}
+}
