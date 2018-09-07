08f47656270bbca21b11706bddefee1de6954e58
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-09-07 20:49:14 +0530

    Create ncharofstring.java

---
+++ beginer/ncharofstring.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int n=sc.nextInt();
+		System.out.println(s.substring(0,n));
+		
+	}
+}
