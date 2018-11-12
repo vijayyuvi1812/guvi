07aea5cd2260ec186821164e62c7f76748e728df
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-11-12 19:53:52 +0530

    Update voworcon.java

--- beginer/voworcon.java
+++ beginer/voworcon.java
@@ -10,7 +10,6 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter any character");
 		char x=sc.next().charAt(0);
 		if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
 		{
