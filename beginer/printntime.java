3fa59425edf7ee52918756c60b1d6b53257c3741
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-11-12 20:02:18 +0530

    Update printntime.java

--- beginer/printntime.java
+++ beginer/printntime.java
@@ -8,10 +8,9 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter how many times u want to print HELLO");
 		int n=sc.nextInt();
 		for(int i=1;i<=n;i++){
-			System.out.println("HELLO");
+			System.out.println("Hello");
 		}
 	}
 }
