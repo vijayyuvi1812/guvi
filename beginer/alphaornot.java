215ff938bf9fe452eeb81a7f1258f049d8dbbbe7
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-11-12 19:56:00 +0530

    Update alphaornot.java

--- beginer/alphaornot.java
+++ beginer/alphaornot.java
@@ -8,13 +8,13 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter any character");
+		
 		char ch=sc.next().charAt(0);
 		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
-			System.out.println("character is alphabet");
+			System.out.println("Alphabet");
 		}
 		else{
-						System.out.println("character is not a alphabet");
+						System.out.println("No");
 			}
 	}
 }
