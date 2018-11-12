8b34a20fe6052ee9b4e620cfc8af6f74e3265808
vijayyuvi1812 <41728656+vijayyuvi1812@users.noreply.github.com>
2018-11-12 19:58:38 +0530

    Update largamng3.java

--- beginer/largamng3.java
+++ beginer/largamng3.java
@@ -8,14 +8,14 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner Sc=new Scanner(System.in);
-		System.out.println("enter num1");
+		
 		int num1=Sc.nextInt();
-		System.out.println("enter num2");
+		
 		int num2=Sc.nextInt();
-		System.out.println("enter num3");
+		
 		int num3=Sc.nextInt();
 		int largest1=num1>num2?num1:num2;
 		int largest=largest1>num3?largest1:num3;
-		System.out.println("largestnumber is"+largest);
+		System.out.println(largest);
 	}
 }
